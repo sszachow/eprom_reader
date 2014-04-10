f33614add288abc140b3728d5ec0b930dde1928a
sszachow <sszachow@gmail.com>
2014-04-10 19:03:26 +0200

    added new dumps; removed txt file writing from dump.py

--- py/dump.py
+++ py/dump.py
@@ -6,7 +6,7 @@ ROOT_DIR = ".."
 DUMPS_DIR = "dumps"
 
 MODULE = 1
-PROM = 2
+PROM = 1
 
 def get_data(addr, ser):
     higher = chr((addr & 0xFF00) >> 8)
@@ -24,17 +24,13 @@ def dump_rom():
     ser.flushOutput()
 
     filename = '%s/%s/module_%s_prom_%s_%s' % (ROOT_DIR, DUMPS_DIR, MODULE, PROM, strftime("%Y%m%d_%H%M%S", localtime()))
-    ftxt = open('%s.txt' % filename, 'w')
     fhex = open('%s.hex' % filename, 'wb')
 
     for addr in xrange(0, 0x3fff + 1):
         data = get_data(addr, ser)
-        char = chr(data) if chr(data) in string.printable[:-5] else '?'
-        ftxt.write("A: 0x%s, D: 0x%s, D: %s, D: %s\n" % (format(addr, '04x'), format(data, '02x'), data, char))
         fhex.write(chr(data))
 
     ser.close()
-    ftxt.close()
     fhex.close()
 
 
